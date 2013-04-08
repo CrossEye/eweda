d4f7a74872f8e9560aafc2774447c330470f4f13
Scott Sauyet <scott.sauyet@gmail.com>
2013-04-07 22:16:00 -0400

    Adding currying, removing attempt at Mike-style lists over arrays.  That might come later.

--- eweda.js
+++ eweda.js
@@ -1,58 +1,28 @@
 (function (root, factory) {if (typeof exports === 'object') {module.exports = factory();} else if (typeof define === 'function' && define.amd) {define(factory);} else {root.eweda = factory();}}(this, function () { // see https://github.com/umdjs/umd/blob/master/returnExports.js
-/*
-    var NIL = function() {
-        return function() {return null;};
-    };
 
-    var makeList = function(arr) {
-        // TODO: non-arrays.
-        if (arr.length === 0) {return NIL}
-        var fn = function(f) {
-            return f(arr, 0, arr.length);
+    var slice = Array.prototype.slice;
+
+    var _ = function(fn) { // should we spell out "curry" or "partial" or leave super-short?
+        var arity = fn.length;
+        var f = function(args) {
+            return function () {
+                var newArgs = args.concat(slice.call(arguments, 0));
+                if (newArgs.length >= arity) {
+                    return fn.apply(this, newArgs)
+                }
+                else return f(newArgs)
+            }
         };
-        fn.toString = function() {
-            return "(" + asArray(this).join(" ") + ")";
-        }
-        return fn;
+        return f([]);
     };
 
-    var asArray = function(list) {
-        var arr = arguments[1] || [];
-        return (list && head(list)) ? (arr.push(head(list)), asArray(tail(list), arr)) : arr;
-    };
-
-    var head = function(list) {
-        return list(function(arr, start, end) {
-            return arr.length ? arr[0] : null;
-        });
-    };
-
-    var tail = function(list) {
-        return list(function(arr, start, end) {
-            return (start < end) ? function(f) {
-                return f(arr, start + 1, end);
-            } : NIL;
-        });
-    };
-
-
-    var foldl = function(fn, acc, list) {
-        return (list === NIL) ? acc : foldl(fn, fn(acc, head(list)), tail(list));
-    };
-
-    return {
-        test: function() {return 42;},
-        foldl: function(fn, acc, arr) {return foldl(fn, acc, makeList(arr))}
-    };
-*/
-
-    var foldl = function(fn, acc, arr) {
+    var foldl = _(function(fn, acc, arr) {
         var total = acc;
         for (var i = 0, len = arr.length; i <len; i++) {
             total = fn(total, arr[i]);
         }
         return total;
-    };
+    });
 
     return {
         foldl: foldl
--- test/test.foldl.js
+++ test/test.foldl.js
@@ -2,8 +2,29 @@ var assert = require("assert");
 var eweda = require("./../eweda");
 
 describe('foldl', function() {
-   it('should fold simple functions over arrays', function() {
-       var fn = function(a, b) {return a + b;}
-       assert.equal(10, eweda.foldl(fn, 0, [1, 2, 3, 4]));
-   });
+    var foldl = eweda.foldl;
+    var add = function(a, b) {return a + b;};
+    var mult = function(a, b) {return a * b;};
+
+    it('should fold simple functions over arrays', function() {
+        assert.equal(10, foldl(add, 0, [1, 2, 3, 4]));
+        assert.equal(24, foldl(mult, 1, [1, 2, 3, 4]));
+    });
+
+    it('should return the accumulator for an empty array', function() {
+        assert.equal(0, foldl(add, 0, []));
+        assert.equal(1, foldl(mult, 1, []));
+    });
+
+    it('should be automatically curried', function() {
+        var sum = foldl(add, 0);
+        assert.equal(10, sum([1, 2, 3, 4]));
+    });
+
+    // TODO:  do we need to use a
+    it.skip('should correctly report the arity of curried versions', function() {
+        var sum = foldl(add, 0);
+        assert.equal(1, sum.length);
+    });
+
 });

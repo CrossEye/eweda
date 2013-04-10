2221b2ee0821ea49403aa87527440b70b3fa53ea
Scott Sauyet <scott@sauyet.com>
2013-04-10 07:41:35 -0400

    Added partials and `reject`

--- test/test.filter.js
+++ test/test.filter.js
@@ -14,3 +14,17 @@ describe('filter', function() {
         assert.deepEqual(onlyEven([1, 2, 3,4, 5, 6, 7]), [2, 4, 6]);
     });
 });
+
+describe('reject', function() {
+    var reject = eweda.reject;
+    var even = function(x) {return !(x % 2);};
+
+    it('should reduce an array to those not matching a filter', function() {
+        assert.deepEqual(reject(even, [1, 2, 3, 4, 5]), [1, 3, 5]);
+    });
+
+    it('should be automatically curried', function() {
+        var odd = reject(even);
+        assert.deepEqual(odd([1, 2, 3,4, 5, 6, 7]), [1, 3, 5, 7]);
+    });
+});

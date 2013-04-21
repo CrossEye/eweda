de55ba5cb6de0e5f06bc92b23e5effad3fc94769
Scott Sauyet <scott.sauyet@gmail.com>
2013-04-20 23:43:17 -0400

    Cleaning up wrap test.

--- test/test.wrap.js
+++ test/test.wrap.js
@@ -1,34 +1,15 @@
-var assert = require("assert");
-var eweda = require("./../eweda");
+var assert = require('assert');
+var eweda = require('./../eweda');
 
 describe('wrap', function() {
-    var wrap = eweda.wrap, reduce = eweda.reduce, zipWith = eweda.zipWith;
-    // TODO: remove this in favor of something simpler.
+    var wrap = eweda.wrap;
+
     it('surrounds the invocation of one function with another', function() {
-        var curves = [11, 4, 18],
-            grades = {
-                alice: [75, 86, 67],
-                bob: [64, 77, 72],
-                carol: [81, 93, 78]
-            },
-            plus = function(a, b) {return a + b;},
-            // TODO: this should be easier
-            avg = function(arr) {return reduce(plus, 0)(arr) / arr.length;},
-            average = function(grades) {
-                var results = {};
-                for (name in grades) {if (grades.hasOwnProperty(name)) {
-                    results[name] = avg(grades[name]);
-                }}
-                return results;
-            },
-            curvedAverage = wrap(average, function(wrapped, grades) {
-                var adjusted = {};
-                for (name in grades) {if (grades.hasOwnProperty(name)) {
-                    adjusted[name] = zipWith(plus, grades[name], curves);
-                }}
-                return wrapped(adjusted);
-            });
-        assert.deepEqual(average(grades), {alice: 76, bob: 71, carol: 84});
-        assert.deepEqual(curvedAverage(grades), {alice: 87, bob: 82, carol: 95});
+        var greet = function(name) {return 'Hello ' + name;};
+        var extendedGreet = wrap(greet, function(gr, name) {
+            return gr('my dear ' + name) + ', how are you?';
+        });
+        assert.equal(greet('joe'), 'Hello joe');
+        assert.equal(extendedGreet('joe'), 'Hello my dear joe, how are you?');
     });
 });

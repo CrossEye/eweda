8cda8a2fb7bf0334bdc0c8b99d563d2744ca5060
Scott Sauyet <scott.sauyet@gmail.com>
2013-04-23 15:48:17 -0300

    Minor formatting tweak

--- eweda.js
+++ eweda.js
@@ -10,10 +10,10 @@
         return (fn.is = fn.are = fn.and = fn);
     };
     var bind = function(fn, context) {
-   		var args = Array.prototype.slice.call(arguments, 2);
-   		return function() {
-   			return fn.apply(context || this, args.concat(Array.prototype.slice.call(arguments)));
-   		};
+        var args = Array.prototype.slice.call(arguments, 2);
+        return function() {
+   	        return fn.apply(context || this, args.concat(Array.prototype.slice.call(arguments)));
+   	    };
    	};
     var slice = bind(Function.prototype.call, Array.prototype.slice);
     var toString = bind(Function.prototype.call, Object.prototype.toString);

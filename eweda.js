e4a769d530e1f8e6a674cbf5fb75cd0b68db744b
Scott Sauyet <scott@sauyet.com>
2013-05-01 08:57:19 -0400

    Adding first pass at documentation, still incomplete

--- eweda.js
+++ eweda.js
@@ -1,12 +1,54 @@
-(function (root, factory) {if (typeof exports === 'object') {module.exports = factory(root);} else if (typeof define === 'function' && define.amd) {define(factory);} else {root.eweda = factory(root);}}(this, function (global) { // see https://github.com/umdjs/umd/blob/master/returnExports.js
+//     eweda.js 0.0.1
+//     https://github.com/CrossEye/eweda
+//     (c) 2013 Scott Sauyet and Michael Hurley
+//     Underscore may be freely distributed under the MIT license.
+
+// Basic Setup
+// -----------
+// Uses a technique from the [Universal Module Definition][umd] to wrap this up for use in Node.js or in the browser,
+// with or without an AMD-style loader.
+//
+//  [umd]: https://github.com/umdjs/umd/blob/master/returnExports.js
+
+(function (root, factory) {if (typeof exports === 'object') {module.exports = factory(root);} else if (typeof define === 'function' && define.amd) {define(factory);} else {root.eweda = factory(root);}}(this, function (global) {
+
+    // The basic implementation of lists is replaceable.  A default one base on arrays is supplied below.
+    // `bootstrap` represents that implementation, which must contain:
+    //
+    //   - `EMPTY`: prototypical (or only) empty list
+    //   - `isAtom`: boolean function which is `true` for non-list, `false` for a list
+    //   - `isEmpty`: boolean function which reports whether a list is empty
+    //   - `prepend`: function which returns a new list with the new element at the front, and the existing elements following
+    //   - `head`: function which returns the first element of a list
+    //   - `tail`: function which returns the rest of the list after the first element
+
     var lib = function(bootstrap) {
+
+        // This function is what is actually returned, with all the functions attached as properties.  That allows
+        //
+        //     var myLib = eweda({
+        //         EMPTY: /* ... */,
+        //         isAtom: /* ... */,
+        //         /* ... */
+        //     });
+        //     var result = myLib.map(someFunction, someCollection);
+        //
+        // where `someCollection` is built to match the style of collection described by the parameter to the first call.
+
         var E = function() {return lib.apply(this, arguments);};
+
+        // Internal Functions and Properties
+        // ---------------------------------
         var undef = (function(){})();
+
+        // Makes an alias for one of the public functions:
         var aliasFor = function(oldName) {
             var fn = function(newName) {E[newName] = E[oldName]; return fn;};
             return (fn.is = fn.are = fn.and = fn);
         };
 
+        // Local copies of the basic list functions supplies in the initial parameter.  Almost all of these become
+        // public.
         var EMPTY = bootstrap.EMPTY;
         var isEmpty = E.isEmpty = bootstrap.isEmpty;
         var prepend = E.prepend = bootstrap.prepend;
@@ -17,33 +59,26 @@
         aliasFor("tail").is("cdr");  // TODO: really? absolutely! without doubt?
         var isAtom = E.isAtom = bootstrap.isAtom;
 
+        // Partial replacement for native `bind`.
         var bind = function(fn, context) {
             var args = Array.prototype.slice.call(arguments, 2);
             return function() {
                 return fn.apply(context || this, args.concat(Array.prototype.slice.call(arguments)));
             };
         };
-
+        // Standard Array and Object methods implemented as pure functions
         var slice = bind(Function.prototype.call, Array.prototype.slice);
         var toString = bind(Function.prototype.call, Object.prototype.toString);
         var isArray = function(val) {return toString(val) === "[object Array]";};
-        var keys = E.keys = function(obj) {
-            var results = [];
-            for (var name in obj) {if (obj.hasOwnProperty(name)) {
-                results.push(name);
-            }}
-            return results;
-        };
-        E.values = function(obj) {
-            return map(props(obj), keys(obj));
-        };
 
+        // Fills out an array to the specified length
         var expand = function(a, len) {
             var arr = a ? isArray(a) ? a : slice(a) : [];
             while(arr.length < len) {arr[arr.length] = undef;}
             return arr;
         };
 
+        // Local version of partial application.
         var _ = function(fn) {
             var arity = fn.length;
             var f = function(args) {
@@ -58,11 +93,97 @@
 
             return f(EMPTY);
         };
+
+        // Internal version of `forEach`.  Possibly to be exposed later.
         var each = _(function(fn, arr) {
             for (var i = 0, len = arr.length; i < len; i++) {
                 fn(arr[i]);
             }
         });
+
+        // Function functions :-)
+        // ----------------------
+
+        // Creates a new function that runs each of the functions supplied as parameters in turn, passing the output
+        // of each one to the next one, starting with whatever arguments were passed to the initial invocation.
+        // Note that if `var h = compose(f, g)`, `h(x)` calls `g(x)` first, passing the result of that to `f()`.
+        var compose = E.compose = function() {  // TODO: type check of arguments?
+            var fns = slice(arguments);
+            return function() {
+                return foldr(function(fn, args) {return [fn.apply(this, args)];}, slice(arguments), fns)[0];
+            }
+        };
+        aliasFor("compose").is("fog"); // TODO: really?
+
+        // Similar to `compose`, but processes the functions in the reverse order so that if if `var h = pipe(f, g)`,
+        // `h(x)` calls `f(x)` first, passing the result of that to `g()`.
+        var pipe = E.pipe = function() { // TODO: type check of arguments?
+            return compose.apply(this, slice(arguments).reverse());
+        };
+        aliasFor("pipe").is("sequence");
+
+        // Returns a new function much like the supplied one except that the first two arguments are inverted.
+        var flip = E.flip = function(fn) {
+            return _(function(a, b) {
+                return fn.apply(this, [b, a].concat(slice(arguments, 2)));
+            });
+        };
+
+        // Creates a new function that acts like the supplied function except that the left-most parameters are
+        // pre-filled.
+        var lPartial = E.lPartial = function (fn) {
+            var args = slice(arguments, 1);
+            return function() {
+                return fn.apply(this, args.concat(slice(arguments)));
+            };
+        };
+        aliasFor("lPartial").is("applyLeft");
+
+        // Creates a new function that acts like the supplied function except that the right-most parameters are
+        // pre-filled.
+        var rPartial = E.rPartial =function (fn) {
+            var args = slice(arguments, 1);
+            return function() {
+                return fn.apply(this, slice(arguments).concat(args));
+            };
+        };
+        aliasFor("rPartial").is("applyRight");
+
+        // Creates a new function that stores the results of running the supplied function and returns those
+        // stored value when the same request is made.  **Note**: this really only handles string and number parameters.
+        E.memoize = function(fn) {
+            var cache = {};
+            return function() {
+                var position = foldl(function(cache, arg) {return cache[arg] || (cache[arg] = {});}, cache,
+                        slice(arguments, 0, arguments.length - 1));
+                var arg = arguments[arguments.length - 1];
+                return (position[arg] || (position[arg] = fn.apply(this, arguments)));
+            };
+        };
+
+        // Wraps a function up in one that will only call the internal one once, no matter how many times the outer one
+        // is called.  ** Note**: this is not really pure; it's mostly meant to keep side-effects from repeating.
+        E.once = function(fn) {
+            var called = false, result;
+            return function() {
+                if (called) {return result;}
+                called = true;
+                return (result = fn.apply(this, arguments));
+            }
+        };
+
+        // Wrap a function inside another to allow you to make adjustments to the parameters or do other processing
+        // either before the internal function is called or with its results.
+        E.wrap = function(fn, wrapper) {
+            return function() {
+                return wrapper.apply(this, [fn].concat(slice(arguments)));
+            };
+        };
+
+        // List Functions
+        // --------------
+
+        // To do
 
         E.and = _(function (a, b) {
             return !!(a && b);
@@ -118,12 +239,6 @@
             return foldr(fn, head(rev), reverse(tail(rev)));
         });
 
-        var flip = E.flip = function(fn) {
-            return _(function(a, b) {
-                return fn.apply(this, [b, a].concat(slice(arguments, 2)));
-            });
-        };
-
         E.append = function(el, arr) {
             return reverse(prepend(el, reverse(arr)));
         };
@@ -155,22 +270,6 @@
         E.reject = _(function(fn, arr) {
             return filter(notFn(fn), arr);
         });
-
-        var lPartial = E.lPartial = function (fn) {
-            var args = slice(arguments, 1);
-            return function() {
-                return fn.apply(this, args.concat(slice(arguments)));
-            };
-        };
-        aliasFor("lPartial").is("applyLeft");
-
-        var rPartial = E.rPartial =function (fn) {
-            var args = slice(arguments, 1);
-            return function() {
-                return fn.apply(this, slice(arguments).concat(args));
-            };
-        };
-        aliasFor("rPartial").is("applyRight");
 
         var prop = E.prop = function(p) {return function(obj) {return obj[p];};};
 
@@ -236,19 +335,6 @@
             };
         };
 
-        var compose = E.compose = function() {  // TODO: type check of arguments?
-            var fns = slice(arguments);
-            return function() {
-                return foldr(function(fn, args) {return [fn.apply(this, args)];}, slice(arguments), fns)[0];
-            }
-        };
-        aliasFor("compose").is("fog"); // TODO: really?
-
-        var pipe = E.pipe = function() { // TODO: type check of arguments?
-            return compose.apply(this, slice(arguments).reverse());
-        };
-        aliasFor("pipe").is("sequence");
-
         var identity = E.identity = function(val) {
             return function() {return val;};
         };
@@ -261,31 +347,15 @@
             return function(prop) {return obj && obj[prop];};
         };
 
-        E.wrap = function(fn, wrapper) {
-            return function() {
-                return wrapper.apply(this, [fn].concat(slice(arguments)));
-            };
+        var keys = E.keys = function(obj) {
+            var results = [];
+            for (var name in obj) {if (obj.hasOwnProperty(name)) {
+                results.push(name);
+            }}
+            return results;
         };
-
-        // note: not really pure.  Meant to keep side-effects from repeating.
-        E.once = function(fn) {
-            var called = false, result;
-            return function() {
-                if (called) {return result;}
-                called = true;
-                return (result = fn.apply(this, arguments));
-            }
-        };
-
-        // note: really only handles string and number parameters
-        E.memoize = function(fn) {
-            var cache = {};
-            return function() {
-                var position = foldl(function(cache, arg) {return cache[arg] || (cache[arg] = {});}, cache,
-                        slice(arguments, 0, arguments.length - 1));
-                var arg = arguments[arguments.length - 1];
-                return (position[arg] || (position[arg] = fn.apply(this, arguments)));
-            };
+        E.values = function(obj) {
+            return map(props(obj), keys(obj));
         };
 
         E.inContext = function(obj) {

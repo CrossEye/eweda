9151c5ecee19551549dcb6533a44de90d427716f
Scott Sauyet <scott.sauyet@gmail.com>
2013-06-08 12:04:50 -0400

    Reverting file accidentally overridden with irrelevant code.

--- examples/lineItems.js
+++ examples/lineItems.js
@@ -1,62 +1,111 @@
-var problem11 = function(text) {
-    var split = function(chars, str) {return str.split(chars);};
-    var directions = [
-        {name: "NE", dr: -1, dc: +1},
-        {name:  "E", dr:  0, dc: +1},
-        {name: "SE", dr: +1, dc: +1},
-        {name:  "S", dr: +1, dc:  0}
-    ];
-    var grid = map(compose(map(Number), lPartial(split, " ")), text.split(/\r?\n/));
-    var rows = size(grid);
-    var cols = rows && size(head(grid));
-    var disallowed = {val: Number.NEGATIVE_INFINITY};
-    if (!all(function(row) {return size(row) === cols;}, grid)) {return disallowed;}
-    var best = disallowed;
-    var curr;
-    var conf = {grid: grid, count: 4, rows: rows, cols: cols};
-    var calculate = function(conf, row, col, dir) {
-        var endRow = row + dir.dr * (conf.count - 1);
-        var endCol = col + dir.dc * (conf.count - 1);
-        if (endRow < 0 || endRow >= rows || endCol < 0 || endCol > cols) {
-            return disallowed;
-        }
-        return {row: row, col: col, dir: dir, val: product(map(function(n) {
-            return grid[row + n * dir.dr][col + n * dir.dc]}, range(0, conf.count))
-        )};
-    };
+//=============================================================================
+// First, some setup:
+//=============================================================================
 
-    for (var row = 0; row < rows; row++) {
-        for (var col = 0; col < cols; col++) {
-            for (var dir = 0, len = directions.length; dir < len; dir++) {
-                curr = calculate(conf, row, col, directions[dir]);
-                best = (curr.val > best.val) ? curr : best;
-            }
-        }
-    }
-    return best;
+var prices = {
+    "rubber ball": 1.00,
+    doll: 3.50,
+    tiddlywinks: .25,
+    "chess board": 2.75,
+    jumprope: 1.00
 };
 
-var text = (function () {/*
-08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
-49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
-81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
-52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
-22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
-24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
-32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
-67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
-24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
-21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
-78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
-16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
-86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
-19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
-04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
-88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
-04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
-20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
-20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
-01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
-*/}).toString().match(/[^]*\/\*\r?\n([^]*)\r?\n\*\/\}$/)[1];
+var stock = {
+    "rubber ball": 4,
+    doll: 27,
+    tiddlywinks: 0,
+    "chess board": 5,
+    jumprope: 7
+};
 
-console.log(problem11(text)); //=> {row: 15, col: 3, dir: "NE", val: 70600674}
+//=============================================================================
+// Then our test data:
+//=============================================================================
+
+var order = [
+    {item: "rubber ball", qty: 3},
+    {item: "jumprope", qty: 8},
+    {item: "doll", qty: 5},
+    {item: "tiddlywinks", qty: 12},
+    {item: "slinky", qty: 3}
+];
+
+//=============================================================================
+// Two helper functions:
+//=============================================================================
+
+var checkLine = function(line) {
+    return (line.item in prices) && (line.item in stock) && stock[line.item] >= line.qty;
+};
+
+var buildLineItem = function(line) {
+    return {item: line.item, price: prices[line.item], qty: line.qty, cost: line.qty * prices[line.item]};
+};
+
+
+//=============================================================================
+// The required output:
+//=============================================================================
+
+//  {
+//      lineItems: [
+//          {
+//              item: "rubber ball",
+//              price: 1.00,
+//              qty: 3,
+//              cost: 3.00
+//          },
+//          {
+//              item: "doll",
+//              price: 3.50,
+//              qty: 5,
+//              cost: 17.50
+//          }
+//      ],
+//      total: 20.50
+//  }
+
+// (Note that items are only selected if there is enough inventory to fill the
+// entire request, and if we have both stock and price entries for the item.
+// [So no slinky.])
+
+
+//=============================================================================
+// And a naive version of the code:
+//=============================================================================
+
+var processOrder1 = function(order) {
+    var lineItems = [], line, price, item, qty, i, len, cost;
+    var total = 0;
+    for (i = 0, len = order.length; i < len; i++) {
+        line = order[i];
+        item = line.item;
+        qty = line.qty;
+        if (checkLine(line)) {
+            lineItems.push(buildLineItem(line));
+            price = prices[item];
+            cost = qty * price;
+            total += cost;
+        }
+    }
+    return {
+        lineItems: lineItems,
+        total: total
+    };
+};
+
+processOrder1(order);
+
+//=============================================================================
+// Certainly this code might be tightened up.  But here it is with eweda:
+//=============================================================================
+
+var processOrder2 = function(order) {
+    var lineItems = map(buildLineItem, filter(checkLine, order));
+    return {
+        lineItems: lineItems,
+        total: reduce(sum, 0, map(prop("cost"), lineItems))
+    };
+};
+
+processOrder2(order);
